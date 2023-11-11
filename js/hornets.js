11357d80091bf3f84a4f4d695afc04ce0dda7dd3
Jason Schmitt <schmitty890@gmail.com>
2023-11-10 20:30:53 -0500

    updating scoreboard

--- js/hornets.js
+++ js/hornets.js
@@ -18,89 +18,92 @@ fetch(weaklyTypedStats)
   .catch((error) => {
     console.error("There was a problem with the fetch operation:", error);
   });
+// call get data for scoreboards on page load
+getDataForScoreBoards();
+// set interval calls for new data every X number of milliseconds
+setInterval(() => {
+  getDataForScoreBoards();
+}, 30000);
 // Fetching events
-
-fetch(weaklyTypedEvents)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json(); // Parse the response as JSON
-  })
-  .then((data) => {
-    // Handle the JSON data in this block
-    // Build more games scoreboard function
-    
-    // buildScoreBoards(data);
-
-
-    // FOR EACH STATEMENT ITERATE OVER GAMES
-    data.scores.forEach((scores) => {
-      const awayTeam = scores.teams.awayTeam;
-      const homeTeam = scores.teams.homeTeam;
-      const homeTeamAbbrev = scores.teams.homeTeam.abbrev;
-      const homeTeamLogo = scores.teams.homeTeam.logo;
-      const homeTeamColor = scores.teams.homeTeam.teamColor;
-      const awayTeamAbbrev = scores.teams.awayTeam.abbrev;
-      const awayTeamLogo = scores.teams.awayTeam.logo;
-      const awayTeamColor = scores.teams.awayTeam.teamColor;
-      const awayTeamScore = scores.teams.awayTeam.score;
-      const homeTeamScore = scores.teams.homeTeam.score;
-
-      // Check if either the awayTeam or homeTeam is the Charlotte Hornets
-      if (awayTeam.abbrev === "CHA" || homeTeam.abbrev === "CHA") {
-        if (awayTeam.abbrev === "CHA") {
-          // If Hornets are away
-          // Injecting away (hornets) team data
-          $(".away-team-name").html(homeTeamAbbrev); // ABR NAME
-          $(".away-team-logo").html(
-            '<img src="' + homeTeamLogo + '" width="50px" height="50px"/>'
-          ); // LOGO
-          $(".away-team").css("background-color", "#" + homeTeamColor); // COLOR
-          $(".away-team-score").html(homeTeamScore); // SCORE
-          // Injecting home team data
-          $(".home-team-name").html(awayTeamAbbrev); // ABR NAME
-          $(".home-team-logo").html(
-            '<img src="' + awayTeamLogo + '" width="50px" height="50px"/>'
-          ); // LOGO
-          $(".home-team").css("background-color", "#" + awayTeamColor); //  COLOR
-          $(".home-team-score").html(awayTeamScore); // SCORE
-          console.log("home team score is: ", awayTeamScore);
-        } 
-        if (homeTeam.abbrev === "CHA") {
-          // if hornets are home
-          $(".away-team-name").html(awayTeamAbbrev); // ABREV
-          $(".away-team-logo").html(
-            '<img src="' + awayTeamLogo + '" width="50px" height="50px"/>'
-          ); // LOGO
-          $(".away-team").css("background-color", "#" + awayTeamColor); // COLOR
-          $(".away-team-score").html(awayTeamScore); // SCORE
-          // Injecting home (hornets) team data
-          $(".home-team-name").html(homeTeamAbbrev); // ABBREV
-          $(".home-team-logo").html(
-            '<img src="' + homeTeamLogo + '" width="50px" height="50px"/>'
-          ); // LOGO
-          $(".home-team").css("background-color", "#" + homeTeamColor); // COLOR
-          $(".home-team-score").html(homeTeamScore); // SCORE
-        }
-        
+function getDataForScoreBoards() {
+  fetch(weaklyTypedEvents)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
-      // else  
-      //   {
-      //     // IF CHA IS NOT FOUND HOME OR AWAY -- NO GAME
-      //     $(".home-team-name").html("GAME");
-      //     $(".away-team-name").html("NO");
+      return response.json(); // Parse the response as JSON
+    })
+    .then((data) => {
+      // Handle the JSON data in this block
+      // Build more games scoreboard function
+
+      buildScoreBoards(data);
+
+      // FOR EACH STATEMENT ITERATE OVER GAMES
+      // data.scores.forEach((scores) => {
+      //   const awayTeam = scores.teams.awayTeam;
+      //   const homeTeam = scores.teams.homeTeam;
+      //   const homeTeamAbbrev = scores.teams.homeTeam.abbrev;
+      //   const homeTeamLogo = scores.teams.homeTeam.logo;
+      //   const homeTeamColor = scores.teams.homeTeam.teamColor;
+      //   const awayTeamAbbrev = scores.teams.awayTeam.abbrev;
+      //   const awayTeamLogo = scores.teams.awayTeam.logo;
+      //   const awayTeamColor = scores.teams.awayTeam.teamColor;
+      //   const awayTeamScore = scores.teams.awayTeam.score;
+      //   const homeTeamScore = scores.teams.homeTeam.score;
+
+      //   // Check if either the awayTeam or homeTeam is the Charlotte Hornets
+      //   if (awayTeam.abbrev === "CHA" || homeTeam.abbrev === "CHA") {
+      //     if (awayTeam.abbrev === "CHA") {
+      //       // If Hornets are away
+      //       // Injecting away (hornets) team data
+      //       $(".away-team-name").html(homeTeamAbbrev); // ABR NAME
+      //       $(".away-team-logo").html(
+      //         '<img src="' + homeTeamLogo + '" width="50px" height="50px"/>'
+      //       ); // LOGO
+      //       $(".away-team").css("background-color", "#" + homeTeamColor); // COLOR
+      //       $(".away-team-score").html(homeTeamScore); // SCORE
+      //       // Injecting home team data
+      //       $(".home-team-name").html(awayTeamAbbrev); // ABR NAME
+      //       $(".home-team-logo").html(
+      //         '<img src="' + awayTeamLogo + '" width="50px" height="50px"/>'
+      //       ); // LOGO
+      //       $(".home-team").css("background-color", "#" + awayTeamColor); //  COLOR
+      //       $(".home-team-score").html(awayTeamScore); // SCORE
+      //       console.log("home team score is: ", awayTeamScore);
+      //     }
+      //     if (homeTeam.abbrev === "CHA") {
+      //       // if hornets are home
+      //       $(".away-team-name").html(awayTeamAbbrev); // ABREV
+      //       $(".away-team-logo").html(
+      //         '<img src="' + awayTeamLogo + '" width="50px" height="50px"/>'
+      //       ); // LOGO
+      //       $(".away-team").css("background-color", "#" + awayTeamColor); // COLOR
+      //       $(".away-team-score").html(awayTeamScore); // SCORE
+      //       // Injecting home (hornets) team data
+      //       $(".home-team-name").html(homeTeamAbbrev); // ABBREV
+      //       $(".home-team-logo").html(
+      //         '<img src="' + homeTeamLogo + '" width="50px" height="50px"/>'
+      //       ); // LOGO
+      //       $(".home-team").css("background-color", "#" + homeTeamColor); // COLOR
+      //       $(".home-team-score").html(homeTeamScore); // SCORE
+      //     }
       //   }
-      
-
+      //   // else
+      //   //   {
+      //   //     // IF CHA IS NOT FOUND HOME OR AWAY -- NO GAME
+      //   //     $(".home-team-name").html("GAME");
+      //   //     $(".away-team-name").html("NO");
+      //   //   }
+      // });
+      console.log("the weakly typed api event data today is", data);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
     });
-    console.log("the weakly typed api event data today is", data);
-  })
-  .catch((error) => {
-    console.error("There was a problem with the fetch operation:", error);
-  });
+}
 
-  // Second events request
+// Second events request
 fetch(weaklyTypedEvents)
   .then((response) => {
     if (!response.ok) {
@@ -168,18 +171,16 @@ fetch(
   });
 // Seperate fetch for more data
 
-
 /**
  * buildLeaderBoards builds the html template for the leaderboards section
  * @param {*} data - data we get back from the api request
  */
 
-
 function buildLeaderBoards(data) {
   // console.log("buildLeaderBoards func");
   const statGroupsOffense = data.stats.offense.groups;
   const statGroupsDefense = data.stats.defense.groups;
-  let html = "";
+  let html = ``;
 
   // we can leave these offense/defense separate for now if we want to separate these into other sections in the future
   //build offense html
@@ -252,81 +253,113 @@ function buildLeaderBoards(data) {
 
 /** Function to build the HTML of extra scoreboards for todays games */
 
+function buildScoreBoards(data) {
+  const moreGames = data.scores;
+  console.log("moreGames");
+  console.log(moreGames);
+  const lastUpdatedHTML = `
+    <div>last updated time: ${moment(data.date).format("h:mm a")}</div>
+  `;
+  $("#moreGames").empty();
 
-// function buildScoreBoards(data) {
-//   const moreGames = data.scores;
-  
-  
+  // Iterate over each game
+  moreGames.forEach((score) => {
+    let html = "";
+    // Define teams
+    const awayTeams = score.teams.awayTeam;
+    const homeTeams = score.teams.homeTeam;
+    let gameStartTime = "";
+    let classes = "";
+    if (score.status.state === "pre") {
+      gameStartTime = `
+        <div class="timer">
+          <div class="timer-container">
+            <div class="quarter">
+              Start time
+            </div>
+            <div class="timeleft">
+              ${moment
+                .utc(score.startTime)
+                .utcOffset("-05:00")
+                .format("ddd, hA")}
+            </div>
+          </div> 
+        </div>`;
+    } else if (score.status.state === "in") {
+      classes += " in-progress";
+      gameStartTime = `
+        <div class="timer">
+          <div class="timer-container">
+            <div class="quarter">
+              ${score.status.detail}
+            </div>
+          </div> 
+        </div>`;
+    } else if (score.tstatus.state === "final") {
+      gameStartTime = `
+        <div class="timer">
+          <div class="timer-container">
+            <div class="quarter">
+              FINAL
+            </div>
+            <div class="timeleft">
+              View Details
+            </div>
+          </div> 
+        </div>`;
+    }
 
-//   // Iterate over each game 
-//   moreGames.forEach((score) => {
-    
-//     let html = "";
-//     // Define teams 
-//     const awayTeams = score.teams.awayTeam;
-//     const homeTeams = score.teams.homeTeam;
+    html += `
+    <div class="scoreboard justify-content-center">
+      <div class="team team-a mg-away-team" style="background-color: #${
+        awayTeams.teamColor
+      };">
+        <div class="mg-away-team-logo">
+          <img src=${awayTeams.logo} width="50px" height="50px" alt=${
+      awayTeams.shortDisplayName
+    } />
+        </div>
+        <div class="team-detail">
+          <div class="team-nameandscore">
+              <div class="mg-away-team-name">
+                ${awayTeams.abbrev}
+              </div>
+              <div class="mg-away-team-score">
+                ${awayTeams.score ? awayTeams.score : "-"}
+              </div>
+          </div>
+          <div class="team-thisgame">
+            ${awayTeams.records[0].summary}
+          </div>
+        </div>
+      </div>
+      <div class="team team-b charlotte mg-home-team" style="background-color: #${
+        homeTeams.teamColor
+      };">
+        <div class="mg-home-team-logo">
+          <img src=${homeTeams.logo} width="50px" height="50px" alt=${
+      homeTeams.shortDisplayName
+    } />
+        </div>
+        <div class="team-detail">
+          <div class="team-nameandscore">
+              <div class="mg-home-team-name">
+                ${homeTeams.abbrev}
+              </div>
+              <div class="mg-home-team-score">
+                ${homeTeams.score ? homeTeams.score : "-"}
+              </div>
+          </div>
+          <div class="team-thisgame">
+            ${homeTeams.records[0].summary}
+          </div>
+        </div>
+      </div>
+      ${gameStartTime}
+      `;
 
-
-//     //thought i needed this but apparently not
-//     // awayTeams.forEach((awayTeam) => {
-//     //   console.log('Away Team:', awayTeam);
-//     // });
-    
-
-//      html += `
-//      <div class="scoreboard justify-content-center">
-//           <div class="team team-a mg-away-team" style="background-color: #${awayTeams.teamColor};">
-//              <div class="mg-away-team-logo">
-//              <img src=${awayTeams.logo} width="50px" height="50px"/>
-//              </div>
-//              <div class="team-detail">
-//                 <div class="team-nameandscore">
-//                    <div class="mg-away-team-name">
-//                    ${awayTeams.abbrev}
-//                    </div>
-//                    <div class="mg-away-team-score">
-//                       0
-//                    </div>
-//                 </div>
-//                 <div class="team-thisgame">
-                   
-//                 </div>
-//              </div>
-//           </div>
-//           <div class="team team-b charlotte mg-home-team" style="background-color: #${homeTeams.teamColor};">
-//              <div class="mg-home-team-logo">
-//                 <img src=${homeTeams.logo} width="50px" height="50px"/>
-//              </div>
-//              <div class="team-detail">
-//                 <div class="team-nameandscore">
-//                    <div class="mg-home-team-name">
-//                       ${homeTeams.abbrev}
-//                    </div>
-//                    <div class="mg-home-team-score">
-//                       0
-//                    </div>
-//                 </div>
-//                 <div class="team-thisgame">
-                   
-//                 </div>
-//              </div>
-//           </div>
-//           <div class="timer">
-//              <div class="timer-container">
-//                  <div class="quarter">
-//                      1st
-//                  </div>
-//                  <div class="timeleft">
-//                      00:00
-//                  </div>
-                 
-//              </div> 
-//           </div>
-//           `;
-       
-//        $("#moreGames").append(html);
-       
-    
-//   });
-// }
+    $("#moreGames").append(html);
+  });
+  $("#moreGames").prepend(lastUpdatedHTML);
+}
 /** @this function under construction */
